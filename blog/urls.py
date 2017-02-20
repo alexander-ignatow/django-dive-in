f7213fffa7d7e37e067eccb05063fdd09467d7a9
Alex Ignatov <alexander.ignatow@gmail.com>
2017-02-20 23:34:55 +0200

    create post page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.feed, name='feed'),
+    url(r'^feed', views.feed, name='feed'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post, name='post'),
-
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 def feed(request):
-    feed = Post.objects.order_by('published_date')
+    feed = Post.objects.order_by('-published_date')
     return render(request, 'blog/feed.html', {'feed': feed})
 
 
 def post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
