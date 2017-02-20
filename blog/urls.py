45b00cd0a6219ca050e6e2e539c960460f673ebe
Alex Ignatov <alexander.ignatow@gmail.com>
2017-02-20 22:44:02 +0200

    post page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.feed, name='feed'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post, name='post'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 def feed(request):
     feed = Post.objects.order_by('published_date')
     return render(request, 'blog/feed.html', {'feed': feed})
+
+
+def post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post.html', {'post': post})
