ecac7aa87e9788e1c1706168c6dafd373c8557bb
Alex Ignatov <alexander.ignatow@gmail.com>
2017-02-20 20:43:21 +0200

    feed

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+sfrom django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def feed(request):
-    return render(request, 'blog/feed.html', {})
+    feed = Post.objects.order_by('published_date')
+    return render(request, 'blog/feed.html', {'feed': posts})
